a52389ede0ceade4fa7e08e52cbc12b2563e7783
SandeepKumar <sandeepkusolanki@gamil.com>
2025-05-15 21:35:58 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [input , setInput] = useState(0);
+  const [sum , setSum]=useState(0);
+  useEffect(()=>{
+    setSum(sum + Number(input))
+  },[input])
   return (
     <div>
         {/* Do not remove the main div */}
+        <input
+          type="number"
+          value={input}
+          onChange={(e)=>setInput(e.target.value)}
+        />
+        <p>Sum: {sum}</p>
     </div>
   )
 }
